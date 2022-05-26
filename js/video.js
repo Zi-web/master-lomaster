11665513a6bfd6c7d0255e7976115ce6e4b27f92
Zi-web <37513694+Zi-web@users.noreply.github.com>
2022-05-26 15:55:25 +0300

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,43 +1,53 @@
-/*let playButton = document.getElementById("play_button");
-playButton.addEventListener("click", function () {
-  if (video.paused == true) {
-    // Play the video
-    video.play();
-
-    // Update the button text to 'Pause'
-    playButton.innerHTML = "Pause";
-  } else {
-    // Pause the video
-    video.pause();
-
-    // Update the button text to 'Play'
-    playButton.innerHTML = "Play";
-  }
-});
+/*
+const video1 = "img/VIDEO-2022-05-04-16-27-40.mp4";
+const video2 = "img/VIDEO-2022-05-04-16-27-40.mp4";
+const video3 = "img/VIDEO-2022-05-04-16-27-40.mp4";
+const video4 = "img/VIDEO-2022-05-04-16-27-40.mp4";
 */
 
-var box = document.querySelector(".video__box");
-var link = document.querySelector(".play");
-var video = document.querySelector(".video__video");
-var close = box.querySelector(".video__stop");
-var overlay = document.querySelector(".video");
-/*let player = video.querySelector("video__video");*/
+/*
+let videoArr = [
+  "img/VIDEO-2022-05-04-16-27-40.mp4",
+  "img/VIDEO-2022-05-04-16-27-40.mp4",
+  "img/VIDEO-2022-05-04-16-27-40.mp4",
+  "img/VIDEO-2022-05-04-16-27-40.mp4",
+];
+*/
 
-link.addEventListener("click", function (event) {
-  event.preventDefault();
-  video.src = "img/VIDEO-2022-05-04-16-27-40.mp4";
-  video.style.display = "inline";
-  close.style.display = "inline";
-  overlay.classList.add("video__open");
-});
+window.onload = function () {
+  const box = document.querySelector(".video__box");
+  const link = document.querySelectorAll(".play");
+  const video = document.querySelectorAll(".video__video");
+  const close = document.querySelectorAll(".video__stop");
+  let overlay = document.querySelector(".video");
 
-close.addEventListener("click", function (event) {
-  event.preventDefault();
-  video.style.display = "none";
-  close.style.display = "none";
-  box.classList.remove("video__video");
-  overlay.classList.remove("video__open");
-  video.src = "";
-  /*video.src = "img/VIDEO-2022-05-04-16-27-40.mp4";*/
-  /*player.muted = true;*/
-});
+  for (let i = 0; i < link.length; i++) {
+    /*console.log(link[i]);*/
+    link[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      //for (let j = 0; j < video.length; j++) {
+
+      //videoArr.forEach(function (item) {
+      console.log(video[i]);
+      video[i].src = video[i].getAttribute("src");
+      console.log(video[i].getAttribute("src"));
+      video[i].style.display = "inline";
+      console.log(video[i]);
+      //video[j].setAttribute("style", "display: inline");
+      close[i].style.display = "inline";
+      video[i].classList.add("video__open");
+      //});
+    });
+
+    close[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      video[i].style.display = "none";
+      close[i].style.display = "none";
+      box.classList.remove("video__video");
+      overlay.classList.remove("video__open");
+      video[i].src = video[i].setAttribute("src", "null");
+      /*video[i].src = null;*/
+      /*player.muted = true;*/
+    });
+  }
+};
